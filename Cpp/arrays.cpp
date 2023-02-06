31ef4121c57f94244567e789ae24f069cb4e7d2f
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2023-02-06 13:51:58 +0800

    Create arrays.cpp

---
+++ Cpp/arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numbers[5];
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+
+    for (int i = 0; i < 5; ++i) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
