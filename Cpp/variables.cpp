a5f8eb7a4a72a8f3fbaba5d42b9b1a0693baf332
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2023-02-06 13:47:43 +0800

    Create variables.cpp

---
+++ Cpp/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    float b = 20.5;
+    char c = 'A';
+
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+    cout << "c: " << c << endl;
+
+    return 0;
+}
